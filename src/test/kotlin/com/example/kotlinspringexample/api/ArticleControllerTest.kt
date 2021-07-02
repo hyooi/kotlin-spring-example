5e0bbd8b362c9f1361816df046d9c6fc520edf99
HK <hyoin01@gmail.com>
2021-07-03 01:28:51 +0900

    test. add httpControllerTest

--- src/test/kotlin/com/example/kotlinspringexample/api/ArticleControllerTest.kt
+++ src/test/kotlin/com/example/kotlinspringexample/api/ArticleControllerTest.kt
@@ -1,5 +1,58 @@
 package com.example.kotlinspringexample.api
 
-import org.junit.jupiter.api.Assertions.*
+import com.example.kotlinspringexample.entity.Article
+import com.example.kotlinspringexample.entity.User
+import com.example.kotlinspringexample.repository.ArticleRepository
+import com.example.kotlinspringexample.repository.UserRepository
+import com.ninjasquad.springmockk.MockkBean
+import io.mockk.every
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.http.MediaType
+import org.springframework.jdbc.JdbcUpdateAffectedIncorrectNumberOfRowsException
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 
-internal class ArticleControllerTest
+@WebMvcTest
+class HttpControllerTest(@Autowired val mockMvc: MockMvc) {
+
+    @MockkBean
+    private lateinit var userRepository: UserRepository
+
+    @MockkBean
+    private lateinit var articleRepository: ArticleRepository
+
+    @Test
+    fun `List articles`() {
+        val juergen = User("springjuergen", "Juergen", "Hoeller")
+        val spring5article = Article("Spring Framework 5.0 goes GA", "Dear Spring community...",
+        "Lorem ipsum", juergen)
+        val spring43Article = Article("Spring Framework 4.3 goes GA", "Dear Spring community...",
+        "Lorem ipsum", juergen)
+        every { articleRepository.findAllByOrderByAddedAtDesc() } returns listOf(spring5article,
+            spring43Article)
+
+        mockMvc.perform(get("/api/article/").accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk)
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("\$.[0].author.login").value(juergen.login))
+            .andExpect(jsonPath("\$.[0].slug").value(spring5article.slug))
+            .andExpect(jsonPath("\$.[1].author.login").value(juergen.login))
+            .andExpect(jsonPath("\$.[1].slug").value(spring43Article.slug))
+    }
+
+    @Test
+    fun `List users`() {
+        val juergen = User("springjuergen", "Juergen", "Hoeller")
+        val smaldini = User("smalidini", "Stephane", "Maldini")
+        every { userRepository.findAll() } returns listOf(juergen, smaldini)
+
+        mockMvc.perform(get("/api/user/").accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk)
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("\$.[0].login").value(juergen.login))
+            .andExpect(jsonPath("\$.[1].login").value(smaldini.login))
+    }
+}
